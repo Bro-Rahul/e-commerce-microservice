896ab667311c4f74393c2d21e2631aed66bdc8de
Bro-Rahul <mr.rahul.0619@gmail.com>
2026-09-25 13:13:33 +0530

    initialize the catalog service

---
+++ catalog/src/test/java/com/e_commerce/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
